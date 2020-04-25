621a891f7e6d02ab604ee41bc20e8c22627f793b
0xBloodHound <bloodhound.sec@gmail.com>
2020-04-25 10:51:08 +0700

    Initial

---
+++ App.js
@@ -0,0 +1,131 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import { fetch } from 'react-native-ssl-pinning';
+
+const App: () => React$Node = () => {
+
+  fetch("https://snoopbees.com", {
+    method: "GET" ,
+    pkPinning: true,
+    sslPinning: {
+        // Run this code, the react server will show the correct keys
+        certs: ["sha256/dNQONYjx86oQjY+O+l03K5VbFHrMmA7mVVucpKWoiiw="] 
+    }
+  }).then(response => {
+    console.log(`Secure Communication with Trusted Server`);
+  })
+  .catch(err => {
+    console.log(`Untrusted Comunication`);
+  })
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # ReactNative-Certificate-Pinning
+
+```bash
+# Setting up
+git clone https://github.com/0xBloodHound/ReactNative-Certificate-Pinning
+cd ReactNative-Certificate-Pinning
+npm install
+cd ios
+
+# pod 'RNSslPinning', :path => '../node_modules/react-native-ssl-pinning/ios'
+pod install
+# pod 'RNSslPinning', :path => '../node_modules/react-native-ssl-pinning'
+
+# Start debugging Server
+cd /path/to/ReactNative-Certificate-Pinning
+npx react-native start
+
+# Run Android Application
+cd /path/to/ReactNative-Certificate-Pinning
+npx react-native run-android
+
+# Run iOS Application, Open the project by xcode
+./ios/ReactNativeCerificatePinning.xcworkspace
+```
